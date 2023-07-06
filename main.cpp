b1fb78b719f997e52b88d656471075132f13cbec
IlyaDid <didenko.ilya@gmail.com>
2023-07-06 13:46:11 +0300

    Added commentary

--- main.cpp
+++ main.cpp
@@ -6,20 +6,66 @@
 #include <boost/property_tree/json_parser.hpp>
 using namespace std;
 namespace pt = boost::property_tree;
-class Transition{
-public:
-    int to;
-    string input;
-    string output;
-};
-class Node{
-public:
-    int state;
-    string input;
-    Node *par;
-    vector<Node> children;
-};
 class MealyFSM{
+private:
+    class Transition{
+    public:
+        int to;
+        string input;
+    };
+    class Node{
+    public:
+        int state;
+        string input;
+        Node *par;
+        vector<Node> children;
+    };
+    void search(Node *n){
+        bool found;
+        Node *ptr;
+        for(int i = 0; i < transitions[n->state].second.size(); i++){
+            ptr = n;
+            found = false;
+            //Checking branch possible transitions
+            while(ptr != NULL){
+                if(ptr->state == transitions[n->state].second[i].to && ptr->input == transitions[n->state].second[i].input){
+                    found = true;
+                    break;
+                }
+                ptr = ptr->par;
+            }
+            //If found adding new transition to the end of the branch
+            if(found == false){
+                Node child;
+                child.state = transitions[n->state].second[i].to;
+                child.input = transitions[n->state].second[i].input;
+                child.par = n;
+                n->children.push_back(child);
+            }
+        }
+        //Initializing search for each child branch
+        for(int i = 0; i < n->children.size(); i++)
+           search(&n->children[i]);
+    }
+    void print(Node *n){ 
+        Node *ptr = n;
+        stack<string> s;
+        //Adding all transitions in branch to stack
+        while(ptr != NULL){
+            s.push(ptr->input);
+            ptr = ptr->par;
+        }
+        //Printing input symbols
+        while(!s.empty()){
+            cout << s.top() << " ";
+            s.pop();
+        }
+        //Initializing print for each child branch
+        for(int i = 0; i < n->children.size(); i++){
+            cout << endl;
+            print(&n->children[i]);
+        }
+    }
 public:
     int initial_state;
     vector<string> states;
@@ -27,28 +73,29 @@ public:
 
     bool ReadFromJson(string filename){
         pt::ptree root;
-
+        //Reading Json file into ptree
         try{
             pt::read_json(filename, root);
         }catch(pt::json_parser::json_parser_error& e1){
             cerr << e1.what() << endl;
             return false;
         }
-
+        //Making an array of transitions from a ptree
         for(pt::ptree::value_type& state : root.get_child("transitions")){
             vector<Transition> arr;
+            //Checking if there were duplicate states in Json file as well as adding states into array
             if(find(states.begin(), states.end(), state.first) == states.end())
                 states.push_back(state.first);
             else{
                 cerr << "Duplicate state: " << state.first << endl;
                 return false;
             }
+            //Copying each transition from ptree to structure
             for(pt::ptree::value_type& input : state.second){
                 Transition tr;
                 try{
                     tr.input = input.first;
                     tr.to = find(states.begin(), states.end(), input.second.get<string>("state")) - states.begin();
-                    tr.output = input.second.get<string>("output");
                 }catch(pt::ptree_bad_path& e2){
                     cerr << e2.what() << endl;
                     return false;
@@ -57,7 +104,7 @@ public:
             }
             transitions.push_back(make_pair(find(states.begin(), states.end(), state.first) - states.begin(), arr));
         }
-
+        //Seeking position of initial_state in states array
         try{
             initial_state = find(states.begin(), states.end(), root.get<string>("initial_state")) - states.begin();
         }catch(pt::ptree_bad_path& e3){
@@ -68,54 +115,15 @@ public:
     }
 
     void InpSeqGen(){
+        //Making a tree root from initial_state
         Node root;
         root.state = initial_state;
         root.input = "";
         root.par = NULL;
+        //Starting recursive search for possible transitions
         search(&root);
+        //Printing input sequences
         print(&root);
-    }
-private:
-    void search(Node *n){
-        bool found;
-        Node *ptr;
-        for(int i = 0; i < transitions[n->state].second.size(); i++){
-            ptr = n;
-            found = false;
-            while(ptr != NULL){
-                if(ptr->state == transitions[n->state].second[i].to && ptr->input == transitions[n->state].second[i].input){
-                    found = true;
-                    break;
-                }
-                ptr = ptr->par;
-            }
-            if(found == false){
-                Node child;
-                child.state = transitions[n->state].second[i].to;
-                child.input = transitions[n->state].second[i].input;
-                child.par = n;
-                //cout << child.state << " " << child.input << " " << child.par->state << endl;
-                n->children.push_back(child);
-            }
-        }
-        for(int i = 0; i < n->children.size(); i++)
-           search(&n->children[i]);
-    }
-    void print(Node *n){ 
-        Node *ptr = n;
-        stack<string> s; 
-        while(ptr != NULL){
-            s.push(ptr->input);
-            ptr = ptr->par;
-        }
-        while(!s.empty()){
-            cout << s.top() << " ";
-            s.pop();
-        }
-        for(int i = 0; i < n->children.size(); i++){
-            cout << endl;
-            print(&n->children[i]);
-        }
     }
 };
 int main(int argc, char *argv[]){
